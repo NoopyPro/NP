641aaf8584dc60fa15aceb0fd87d30c6ea9c5ad5
NoopyPro <164927543+NoopyPro@users.noreply.github.com>
2024-03-26 22:32:20 +0200

    Update items.py

---
+++ items.py
@@ -0,0 +1,9 @@
+class usable:
+
+    def __init__(self, heal, stamina_gain, mana_gain):
+        self.heal = heal
+        self.stamina_gain = stamina_gain
+        self.mana_gain = mana_gain
+
+class weapon:
+    
